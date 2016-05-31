2ba3fc68eb575c45763e87ffa400ba5e66ddac76
Youri Chcouropat <youri.chcouropat@etu.univ-nantes.fr>
2016-05-31 15:39:06 +0200

    add rest request

--- js/controller.js
+++ js/controller.js
@@ -2,7 +2,7 @@
 angular.module('meteoApp')
     .controller('meteoController', function($scope, Restangular) {
 
-
         _.contains = _.includes;
-        $scope.villes = Restangular.all('weather?q=Nantes&appid=dd81659090b16aafb5e4a2c7a176b6af').getList().$object;
+        $scope.villes = Restangular.one('weather?q=Nantes&appid=dd81659090b16aafb5e4a2c7a176b6af').get();
+        console.lof($scope.villes);
 });
